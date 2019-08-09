ee639c33fedc2ae7060de13e5981f68586885061
Raj <r.bhadauria7@gmail.com>
2019-08-09 03:11:45 +0100

    second commit

--- S3_Upload/__init__.py
+++ S3_Upload/__init__.py
@@ -1,23 +1,25 @@
 
+
 from flask import Flask, render_template, request, redirect
 from werkzeug.utils import secure_filename
 from S3_Upload.users.forms import Update_image
 from config import Config
+import os
 
 
-app = Flask(__name__)
+#app = Flask(__name__)
+
+app = Flask(__name__, instance_relative_config=True)
+
+# CONFIG
 
 app.config.from_object(Config)
 
-#app.secret_key = 'secret'
 
-#### BLUEPRINT CONFIGS #######
 
-from S3_Upload.users.routes import users
 
-# Register the apps
 
-app.register_blueprint(users)
+
 
 ########################################################################################
 
@@ -32,4 +34,22 @@ app.register_blueprint(users)
 
 ########################################################################################
 
+from S3_Upload.tools import upload_file_to_s3
 
+ALLOWED_EXTENSIONS = app.config["ALLOWED_EXTENSIONS"]
+
+
+# ROUTES
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+
+#### BLUEPRINT CONFIGS #######
+
+from S3_Upload.users.routes import users
+
+# Register the apps
+
+app.register_blueprint(users)
--- S3_Upload/users/routes.py
+++ S3_Upload/users/routes.py
@@ -1,6 +1,20 @@
+import os
 from flask import Flask, render_template, request, redirect, Blueprint
 from werkzeug.utils import secure_filename
 from S3_Upload.users.forms import Update_image
+
+
+########################################################################################
+
+from S3_Upload.tools import upload_file_to_s3
+
+
+
+
+# ROUTES
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 
